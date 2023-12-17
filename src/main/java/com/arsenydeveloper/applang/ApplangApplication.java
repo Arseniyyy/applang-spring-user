be228fbc25494565c00bc499eb8b63abba95a5dd
arseniyyy <arskosh05@mail.ru>
2023-12-17 15:25:34 +0700

    Initial

---
+++ src/main/java/com/arsenydeveloper/applang/ApplangApplication.java
@@ -0,0 +1,13 @@
+package com.arsenydeveloper.applang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arsenydeveloper/applang/ApplangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsenydeveloper.applang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
