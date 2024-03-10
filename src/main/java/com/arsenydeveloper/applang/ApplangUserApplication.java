cacd9fab65be8796395c9db88e9aea27333fcb3a
arseniyyy <arskosh05@mail.ru>
2024-03-10 14:39:22 +0700

    Удалил `sql` скрипт, инициализирующий сущность `U`

--- src/main/java/com/arsenydeveloper/applang/ApplangUserApplication.java
+++ src/main/java/com/arsenydeveloper/applang/ApplangUserApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApplangUserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApplangUserApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(ApplangUserApplication.class, args);
 	}
 }
